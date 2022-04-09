61fc0bea283cb5ae7d4c33c15c45b9451edbc87d
Justin <justinjchester@icloud.com>
2022-04-09 17:50:16 -0400

    Initial Commit

---
+++ Workouts/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Workouts
+//
+//  Created by Justin Chester on 2022-04-09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Workouts/WorkoutsApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkoutsApp.swift
+//  Workouts
+//
+//  Created by Justin Chester on 2022-04-09.
+//
+
+import SwiftUI
+
+@main
+struct WorkoutsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
